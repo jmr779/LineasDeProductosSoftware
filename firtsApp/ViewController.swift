18c67b9c7edada37a659aa954543d2d3bbcdb953
Aula11 <aula11@imac-20.local>
2018-09-24 18:04:12 +0200

    FirstApp

--- firtsApp/ViewController.swift
+++ firtsApp/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: properties
+    
+    @IBOutlet weak var nombreLbl: UILabel!
+    @IBOutlet weak var nombreTxt: UITextField!
+    
+    @IBAction func setResetBtn(sender: AnyObject) {
+        nombreLbl.text = "Nombre ???";
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nombreTxt.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool{ // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        nombreLbl.text = textField.text
+    }
 
 }
 
