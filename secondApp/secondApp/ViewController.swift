31f3edd7e8a1700a58935e942cd4cd37011136ca
Adrián Rodríguez Escudero <adrianrodriguezescudero@MacBook-Pro-de-Adrian.local>
2018-10-18 21:46:11 +0200

    secondApp

---
+++ secondApp/secondApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  secondApp
+//
+//  Created by Aula11 on 8/10/18.
+//  Copyright © 2018 asaAdrián Rodríguez Escudero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var imgView: UIImageView!
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
